b5160b0a48ca725159ac37ed1ebdaab056cc0637
Brennan Cheung <git@brennancheung.com>
2017-03-06 21:23:33 -0800

    Implement APIs for authentication microservice

--- src/server/__tests__/api.spec.js
+++ src/server/__tests__/api.spec.js
@@ -1,116 +1,291 @@
 const fs = require('fs')
 const server = require('../server')
 const request = require('superagent')
+const ObjectID = require('mongodb').ObjectID
 
 const secretPath = '/run/secrets/authenticate-secret-key'
 
 console.log(`Loading secret from ${secretPath}`)
 const secret = fs.readFileSync(secretPath, 'utf8').trim()
 
-function noAuthGet (url) {
-  return new Promise((resolve, reject) => {
-    request.get(`http://localhost${url}`).end((err, res) => {
-      resolve(res)
-    })
-  })
-}
-
 function get (url) {
   return new Promise((resolve, reject) => {
     request
       .get(`http://localhost${url}`)
-      .set('x-authenticate-secret', secret)
       .end((err, res) => {
-      resolve(res)
-    })
+        resolve(res)
+      })
+  })
+}
+
+function post (url, data) {
+  return new Promise((resolve, reject) => {
+    request
+      .post(`http://localhost${url}`)
+      .send(data)
+      .end((err, res) => {
+        resolve(res)
+      })
+  })
+}
+
+function put (url, data) {
+  return new Promise((resolve, reject) => {
+    request
+      .put(`http://localhost${url}`)
+      .send(data)
+      .end((err, res) => {
+        resolve(res)
+      })
+  })
+}
+
+function _delete (url, data) {
+  return new Promise((resolve, reject) => {
+    request
+      .delete(`http://localhost${url}`)
+      .end((err, res) => {
+        resolve(res)
+      })
   })
 }
 
 describe('api', () => {
   beforeAll(async () => {
     await server.startServer()
+  })
+
+  beforeEach(async () => {
     await server.cleanDb()
   })
 
-  describe.skip('tenant', () => {
-    it('create a tenant', () => {
-      expect(false).toBe(true)
+  describe('tenant', () => {
+    describe('create tenant', () => {
+      it('requires a name', async () => {
+        const response = await (post('/tenants', {}))
+        expect(response.status).toBe(400)
+      })
+
+      it('successfully creates a new tenant', async () => {
+        const response = await post('/tenants', {name: 'A Tenant'})
+        expect(response.status).toBe(201)
+        expect(response.body.tenant._id).toBeDefined()
+      })
+
+      it('does not allow duplicate tenants', async () => {
+        await (post('/tenants', { name: 'existing' }))
+        const response = await (post('/tenants', { name: 'existing' }))
+        expect(response.status).toBe(409)
+      })
     })
 
-    it('fails when creating a duplicate tenant', () => {
-      expect(false).toBe(true)
+    it('list tenants', async () => {
+      await post('/tenants', { name: 'Tenant 1' })
+      await post('/tenants', { name: 'Tenant 2' })
+      const response = await get('/tenants')
+      expect(response.status).toBe(200)
+      expect(response.body.tenants.length).toBe(2)
+      expect(response.body.tenants[1].name).toBe('Tenant 2')
+      expect(response.body.tenants[1]._id).toBeDefined()
     })
 
-    it('rename a tenant', () => {
-      expect(false).toBe(true)
+
+    describe('update tenant', () => {
+      it('fails when renaming a tenant that does not exist', async () => {
+        await post('/tenants', { name: 'Existing Tenant' })
+        const response = await put('/tenants/12345', { name: 'changed name' })
+        expect(response.status).toBe(404)
+      })
+
+      it('rename a tenant', async () => {
+        const existingTenant = (await post('/tenants', { name: 'Existing Tenant' })).body.tenant
+        const response = await put(`/tenants/${existingTenant._id}`, { name: 'changed name' })
+        expect(response.status).toBe(200)
+        const fetchedTenants = (await get('/tenants')).body.tenants
+        expect(fetchedTenants[0].name).toBe('changed name')
+      })
     })
 
-    if('fails when renaming a tenant that does not exist', () => {
-      expect(false).toBe(true)
-    })
+    describe('delete a tenant', async () => {
+      it('fail when deleting non-existant tenant', async () => {
+        const response = await _delete('/tenants/1234')
+        expect(response.status).toBe(404)
+      })
 
-    it('delete a tenant', () => {
-      expect(false).toBe(true)
-    })
-
-    it('fails when deleting a tenant that does not exist', () => {
-      expect(false).toBe(true)
+      it('delete a tenant', async () => {
+        const existingTenant = (await post('/tenants', { name: 'Existing Tenant' })).body.tenant
+        const response = await _delete(`/tenants/${existingTenant._id}`)
+        expect(response.status).toBe(200)
+        const fetchedTenants = (await get('/tenants')).body.tenants
+        expect(fetchedTenants.length).toBe(0)
+      })
     })
   })
 
-  describe.skip('user', () => {
-    it('create a user', () => {
-      expect(false).toBe(true)
+  describe('user', () => {
+    let tenant
+
+    beforeEach(async () => {
+      tenant = (await post('/tenants', { name: 'a tenant' })).body.tenant
     })
 
-    it('fails when creating a duplicate user', () => {
-      expect(false).toBe(true)
+    describe('create user', () => {
+      it('fails when there is not a valid tenant, username, and password', async () => {
+        const noTenantResponse = await post('/users', { username: 'asdf@asdf.com', password: 'foobar123!' })
+        expect(noTenantResponse.status).toBe(400)
+
+        const noUsernameResponse = await post('/users', { tenant: tenant._id, password: 'foobar123!' })
+        expect(noUsernameResponse.status).toBe(400)
+
+        const noPasswordResponse = await post('/users', { tenant: tenant._id, username: 'asdf@asdf.com' })
+        expect(noPasswordResponse.status).toBe(400)
+      })
+
+      it('creates a user', async () => {
+        const response = await post('/users', { tenantId: tenant._id, username: 'user@domain.com', password: 'foobar123!' })
+        expect(response.status).toBe(201)
+        expect(response.body.user).toBeDefined()
+        expect(response.body.user.username).toBe('user@domain.com')
+      })
+
+      it('does not allow a duplicate username on the same tenant', async () => {
+        await post('/users', { tenantId: tenant._id, username: 'user@domain.com', password: 'foobar123!' })
+        const response = await post('/users', { tenantId: tenant._id, username: 'user@domain.com', password: 'foobar123!' })
+        expect(response.status).toBe(409)
+        const fetchedUsers = (await get('/users')).body.users
+        expect(fetchedUsers.length).toBe(1)
+      })
+
+      it('allows the same username on a different tenant', async () => {
+        const tenant2 = (await post('/tenants', { name: 'another tenant' })).body.tenant
+        await post('/users', { tenantId: tenant._id, username: 'user@domain.com', password: 'foobar123!' })
+        const response = await post('/users', { tenantId: tenant2._id, username: 'user@domain.com', password: 'foobar123!' })
+        expect(response.status).toBe(201)
+        const fetchedUsers = (await get('/users')).body.users
+        expect(fetchedUsers.length).toBe(2)
+      })
     })
 
-    it('allows creating a user with the same username on a different tenant', () => {
-      expect(false).toBe(true)
+    describe('update user', () => {
+      it('fails when updating a user that does not exist', async () => {
+        const response = await put('/users/12345', { password: 'changedPassword' })
+        expect(response.status).toBe(404)
+      })
+
+      it('updates a user', async () => {
+        const existingUser = (await post('/users', { tenantId: tenant._id, username: 'user@domain.com', password: 'foobar123!' })).body.user
+        const response = await put(`/users/${existingUser._id}`, { username: 'changedUsername', password:' changedPassword' })
+        expect(response.status).toBe(200)
+        const fetchedUsers = (await get('/users')).body.users
+        expect(fetchedUsers.length).toBe(1)
+        expect(fetchedUsers[0].username).toBe('changedUsername')
+      })
     })
 
-    it('fail when trying to add a user to a tenant that does not exist', () => {
-      expect(false).toBe(true)
+    describe('delete user', () => {
+      it('fails when trying to delete a user that does not exist', async () => {
+        const response = await _delete('/users/1234')
+        expect(response.status).toBe(404)
+      })
+
+      it('delete a user', async () => {
+        const existingUser = (await post('/users', { tenantId: tenant._id, username: 'user@domain.com', password: 'foobar123!' })).body.user
+        const response = await _delete(`/users/${existingUser._id}`)
+        expect(response.status).toBe(200)
+        const fetchedUsers = (await get('/users')).body.users
+        expect(fetchedUsers.length).toBe(0)
+      })
     })
 
-    it('delete a user', () => {
-      expect(false).toBe(true)
+    describe('user passwords', () => {
+      it('only allows legitimate and secure passwords to be set', async () => {
+        const response = await post('/users', { tenantId: tenant._id, username: 'user@domain.com', password: '123' })
+        expect(response.status).toBe(400)
+        expect(response.body.error).toEqual('password must be at least 6 characters')
+      })
+
+      it('should not return the hashedPassword when requesting users', async () => {
+        const user = (await post('/users', { tenantId: tenant._id, username: 'user@domain.com', password: 'foobar123!' })).body.user
+        expect(user._id).toBeDefined()
+        expect(user.password).not.toBeDefined()
+        expect(user.hashedPassword).not.toBeDefined()
+      })
+
+      it('hashes the password', async () => {
+        const user = (await post('/users', { tenantId: tenant._id, username: 'user@domain.com', password: 'foobar123!' })).body.user
+        const fetchedUser = await server.findOne('users', { _id: ObjectID(user._id) })
+        expect(fetchedUser).toBeDefined()
+        expect(fetchedUser.password).not.toBeDefined()
+        expect(fetchedUser.hashedPassword).toBeDefined()
+        expect(fetchedUser.hashedPassword.length).toBeGreaterThan(10)
+      })
+
+      it('change password on a user', async () => {
+        const user = (await post('/users', { tenantId: tenant._id, username: 'user@domain.com', password: 'foobar123!' })).body.user
+        const fetchedUser = await server.findOne('users', { _id: ObjectID(user._id) })
+        const oldHashedPassword = fetchedUser.hashedPassword
+        expect(oldHashedPassword.length).toBeGreaterThan(10)
+        const response = await put(`/users/${user._id}`, { username: 'changedUsername', password:' changedPassword' })
+        const fetchedUser2 = await server.findOne('users', { _id: ObjectID(user._id) })
+        expect(fetchedUser2.username).toBe('changedUsername')
+        expect(fetchedUser2.hashedPassword.length).toBeGreaterThan(10)
+        expect(fetchedUser2.hashedPassword).not.toEqual(oldHashedPassword)
+      })
+
+      it('changed password must follow same validation rules', async () => {
+        const user = (await post('/users', { tenantId: tenant._id, username: 'user@domain.com', password: 'foobar123!' })).body.user
+        const response = await put(`/users/${user._id}`, { username: 'changedUsername', password: '123' })
+        expect(response.status).toBe(400)
+        expect(response.body.error).toBe('password must be at least 6 characters')
+      })
+
+      it('updates do not require a password', async () => {
+        const user = (await post('/users', { tenantId: tenant._id, username: 'user@domain.com', password: 'foobar123!' })).body.user
+        const fetchedUser = await server.findOne('users', { _id: ObjectID(user._id) })
+        const oldHashedPassword = fetchedUser.hashedPassword
+        expect(oldHashedPassword.length).toBeGreaterThan(10)
+        const response = await put(`/users/${user._id}`, { username: 'changedUsername' })
+        const fetchedUser2 = await server.findOne('users', { _id: ObjectID(user._id) })
+        expect(fetchedUser2.username).toBe('changedUsername')
+        expect(fetchedUser2.hashedPassword.length).toBeGreaterThan(10)
+        expect(fetchedUser2.hashedPassword).toEqual(oldHashedPassword)
+      })
     })
 
-    it('change password on a user', () => {
-      expect(false).toBe(true)
-    })
-
-    it('users are scoped to a tenant', () => {
-      expect(false).toBe(true)
-    })
-
-    it('only allows legitimate and secure passwords to be set', () => {
-      expect(false).toBe(true)
-    })
   })
 
   describe('authentication', () => {
-    it.only('all APIs fail without a valid secret key', async () => {
-      const response1 = await noAuthGet('/tenants')
-      expect(response1.status).toBe(401)
+    let tenant
 
-      const response2 = await get('/tenants')
-      expect(response2.status).toBe(200)
+    beforeEach(async () => {
+      tenant = (await post('/tenants', { name: 'a tenant' })).body.tenant
     })
 
-    it('fails authentication when a user does not exist', () => {
-      expect(false).toBe(true)
+    it('fails authentication when a user does not exist', async () => {
+      const response = await post('/authenticate', { tenantId: tenant._id, username: 'doesNotExist@domain.com', password: 'foobar123!' })
+      expect(response.status).toBe(401)
     })
 
-    it('fails authentication when a user exists but the password is invalid', () => {
-      expect(false).toBe(true)
+    it('fails authentication when a user exists but the password is invalid', async () => {
+      const user = (await post('/users', { tenantId: tenant._id, username: 'user@domain.com', password: 'foobar123!' })).body.user
+      const response = await post('/authenticate', { tenantId: tenant._id, username: 'user@domain.com', password: 'badPassword' })
+      expect(response.status).toBe(401)
     })
 
-    it('generates a JWT when authentication is successful', () => {
-      expect(false).toBe(true)
+    it('authenticates when a user exists and the password is valid', async () => {
+      const user = (await post('/users', { tenantId: tenant._id, username: 'user@domain.com', password: 'foobar123!' })).body.user
+      const response = await post('/authenticate', { tenantId: tenant._id, username: 'user@domain.com', password: 'foobar123!' })
+      expect(response.status).toBe(200)
+    })
+
+    it('generates a JWT when authentication is successful', async () => {
+      const user = (await post('/users', { tenantId: tenant._id, username: 'user@domain.com', password: 'foobar123!' })).body.user
+      const response = await post('/authenticate', { tenantId: tenant._id, username: 'user@domain.com', password: 'foobar123!' })
+      const token = response.body.token
+      expect(token.length).toBeGreaterThan(10)
+      const decoded = server.decodeToken(token)
+      expect(decoded.tenantId).toBe(user.tenantId)
+      expect(decoded.username).toBe(user.username)
     })
   })
 })
--- src/server/server.js
+++ src/server/server.js
@@ -2,7 +2,10 @@ const fs = require('fs')
 const express = require('express')
 const http = require('http')
 const MongoClient = require('mongodb').MongoClient
+const ObjectID = require('mongodb').ObjectID
 const bodyParser = require('body-parser')
+const bcrypt = require('bcrypt')
+const jwt = require('jwt-simple')
 
 const mongoUrl = 'mongodb://mongo/authenticate'
 const port = process.env.VG_AUTHENTICATION_PORT || 80
@@ -10,7 +13,7 @@ const port = process.env.VG_AUTHENTICATION_PORT || 80
 // This secret should be created before the microservice is started.
 const secretPath = '/run/secrets/authenticate-secret-key'
 
-console.log(`Loading secret from ${secretPath}`)
+// console.log(`Loading secret from ${secretPath}`)
 const secret = fs.readFileSync(secretPath, 'utf8');
 
 if (!secret) {
@@ -56,19 +59,130 @@ function remove (collectionName, _id) {
   })
 }
 
-function insert (collectionName, obj) {
+function insertOne (collectionName, obj) {
   return new Promise((resolve, reject) => {
+    const collection = db.collection(collectionName)
+    collection.insertOne(obj, (err, result) => {
+      if (err) {
+        return reject(err)
+      }
+      resolve(result.ops[0])
+    })
   })
 }
 
-function update (collectionName, _id) {
+function find (collectionName, query) {
   return new Promise((resolve, reject) => {
+    const collection = db.collection(collectionName)
+    collection.find(query).toArray((err, result) => {
+      if (err) {
+        return reject(err)
+      }
+      resolve(result)
+    })
   })
+}
+
+function findOne (collectionName, query) {
+  return new Promise((resolve, reject) => {
+    const collection = db.collection(collectionName)
+    collection.findOne(query, (err, result) => {
+      if (err) {
+        return reject(err)
+      }
+      resolve(result)
+    })
+  })
+}
+
+function updateOne (collectionName, _id, updateParams) {
+  return new Promise((resolve, reject) => {
+    const collection = db.collection(collectionName)
+    collection.updateOne({ _id: ObjectID(_id) }, { $set: updateParams }, (err, result) => {
+      if (err) {
+        return reject(err)
+      }
+      resolve(result)
+    })
+  })
+}
+
+function deleteOne(collectionName, filterQuery) {
+  const collection = db.collection(collectionName)
+  return new Promise((resolve, reject) => {
+    collection.deleteOne(filterQuery, (err, result) => {
+      if (err) {
+        return reject(err)
+      }
+      resolve(result)
+    })
+  })
+}
+
+function sanitizeUser (params) {
+  return objRemoveKeys(params, ['password', 'hashedPassword'])
 }
 
 async function createIndexes () {
   // TODO: add indexes to make things faster
   return null
+}
+
+function objOnly (obj, allowedKeys) {
+  let newObj = {}
+
+  allowedKeys.forEach(key => {
+    newObj[key] = obj[key]
+  })
+  return newObj
+}
+
+function objRemoveKeys (obj, keys) {
+  let newObj = {}
+
+  Object.keys(obj)
+    .filter(key => keys.indexOf(key) === -1)
+    .forEach(key => {
+      newObj[key] = obj[key]
+    })
+  return newObj
+}
+
+function hashPassword (password, bcryptRounds=8) {
+  return new Promise( (resolve, reject) => {
+    if (password === null || password === undefined) {
+      return reject(new Error('password not specified'))
+    }
+    bcrypt.genSalt(bcryptRounds, (err, salt) => {
+      if (err) throw err
+      bcrypt.hash(password, salt, (err, hash) => {
+        if (err) throw err
+        resolve(hash)
+      })
+    })
+  })
+}
+
+function verifyPassword (password, hashedPassword) {
+  return new Promise( async (resolve, reject) => {
+    try {
+      bcrypt.compare(password, hashedPassword, (err, res) => {
+        if (err) { return reject(err) }
+        if (!res) { return resolve(null) }
+        return resolve(true)
+      })
+    } catch (err) {
+      return resolve(false)
+    }
+  })
+}
+
+const objHasAllKeys = (obj, keys) => keys.every(key => obj[key] !== undefined)
+
+async function createTenant (params) {
+  const filteredParams = objOnly(params, ['name'])
+  const response = await insertOne('tenants', filteredParams)
+  return response
 }
 
 async function startServer () {
@@ -88,40 +202,210 @@ async function startServer () {
     app.use(bodyParser.json())
 
     app.use((req, res, next) => {
-      console.info(`${req.method} ${req.url}`)
-
-      // perform the authentication
-      const headerKey = req.headers && req.headers['x-authenticate-secret']
-      if (headerKey) {
-        next()
-      } else {
-        res.status(401).send('unauthorized')
-      }
+      // console.info(`${req.method} ${req.url}`)
+      next()
     })
 
-    app.get('/tenants', (req, res) => {
+
+    // LIST TENANTS
+    app.get('/tenants', async (req, res) => {
       // TODO: send lists of tenants
-      res.status(200).send({tenants: []})
+      const tenants = await find('tenants', {})
+      res.status(200).send({ tenants })
     })
 
-    app.post('/tenants', (req, res) => {
-      // TODO: create the tenant
-      if (false) {
+
+    // CREATE TENANT
+    app.post('/tenants', async (req, res) => {
+      const requiredParams = ['name']
+
+      if (!objHasAllKeys(req.body, requiredParams)) {
+        return res.status(400).send({ error: 'missing required params', requiredParams })
+      }
+
+      const findDuplicate = await findOne('tenants', { name: req.body.name })
+
+      if (findDuplicate) {
         return res.status(409).send({error: 'tenant already exists'})
       }
 
-      res.status(201).send({tenant: {_id: 123, name: 'foo'}})
+      const newTenant = await createTenant({ name: req.body.name })
+
+      if (newTenant) {
+        res.status(201).send({tenant: newTenant})
+      } else {
+        res.status(500).send({ error: 'Unable to create tenant for unknown reason.' })
+      }
     })
 
-    app.put('/tenants/:id', (req, res) => {
-      if (false) {
+
+    // UPDATE TENANT
+    app.put('/tenants/:id', async (req, res) => {
+      const id = req.params.id
+      if (!id || id.length !== 24) {
+        return res.status(404).send({ error: 'invalid id' })
+      }
+
+      const tenant = await findOne('tenants', { _id: ObjectID(req.params.id) })
+
+      if (!tenant) {
         return res.status(404).send({error: 'tenant not found'})
       }
-      res.status(200).send({tenant: {_id: 123, name: 'foo'}})
+
+      const updated = await updateOne('tenants', req.params.id, req.body)
+      const updatedTenant = await findOne('tenants', { _id: ObjectID(req.params.id) })
+
+      res.status(200).send({ tenant: updatedTenant })
     })
 
-    app.put('/tenants/:id', (req, res) => {
-      res.status(200).send({tenant: {_id: 123, name: 'foo'}})
+
+    // DELETE TENANT
+    app.delete('/tenants/:id', async (req, res) => {
+      const id = req.params.id
+      if (!id || id.length !== 24) {
+        return res.status(404).send({ error: 'invalid id' })
+      }
+      const tenant = await findOne('tenants', { _id: ObjectID(req.params.id) })
+      if (!tenant) {
+        return res.status(404).send({error: 'tenant not found'})
+      }
+      const deletedTenant = await deleteOne('tenants', { _id: ObjectID(req.params.id) })
+      res.status(200).send({ status: 'success' })
+    })
+
+
+    // LIST USERS
+    app.get('/users', async (req, res) => {
+      const users = await find('users', {})
+      const sanitizedUsers = users.map(sanitizeUser)
+      res.status(200).send({ users: sanitizedUsers })
+    })
+
+
+    // CREATE USER
+    app.post('/users', async (req, res) => {
+      const requiredParams = ['tenantId', 'username', 'password']
+
+      if (!objHasAllKeys(req.body, requiredParams)) {
+        return res.status(400).send({ error: 'missing required params', requiredParams })
+      }
+
+      const tenantId = req.body.tenantId
+      if (!tenantId || tenantId.length !== 24) {
+        return res.status(404).send({ error: 'invalid tenantId' })
+      }
+
+      const params = {
+        tenantId: ObjectID(tenantId),
+        username: req.body.username,
+        password: req.body.password
+      }
+
+      if (params.password.length < 6) {
+        return res.status(400).send({ error: 'password must be at least 6 characters' })
+      }
+
+      const findDuplicate = await findOne('users', { tenantId: params.tenantId, username: params.username })
+      if (findDuplicate) {
+        return res.status(409).send({error: 'username already exists on that tenant'})
+      }
+
+      const userParams = {
+        tenantId: params.tenantId,
+        username: params.username,
+        hashedPassword: await hashPassword(params.password)
+      }
+
+      const newUser = await insertOne('users', userParams)
+      if (newUser) {
+        res.status(201).send({ user: sanitizeUser(newUser) })
+      } else {
+        res.status(500).send({ error: 'Unable to create user for unknown reason.' })
+      }
+    })
+
+
+    // UPDATE USER
+    app.put('/users/:id', async (req, res) => {
+      const id = req.params.id
+      if (!id || id.length !== 24) {
+        return res.status(404).send({ error: 'invalid id' })
+      }
+
+      const user = await findOne('users', { _id: ObjectID(req.params.id) })
+
+      if (!user) {
+        return res.status(404).send({error: 'user not found'})
+      }
+
+      let params = {
+        username: req.body.username
+      }
+
+      if (req.body.password) {
+        if (req.body.password.length < 6) {
+          return res.status(400).send({ error: 'password must be at least 6 characters' })
+        }
+        params.hashedPassword = await hashPassword(req.body.password)
+      }
+
+      const updated = await updateOne('users', req.params.id, params)
+      const updatedUser = await findOne('users', { _id: ObjectID(req.params.id) })
+
+      res.status(200).send({ user: sanitizeUser(updatedUser) })
+    })
+
+
+    // DELETE USER
+    app.delete('/users/:id', async (req, res) => {
+      const id = req.params.id
+      if (!id || id.length !== 24) {
+        return res.status(404).send({ error: 'invalid id' })
+      }
+      const user = await findOne('users', { _id: ObjectID(req.params.id) })
+      if (!user) {
+        return res.status(404).send({error: 'user not found'})
+      }
+      const deletedUser = await deleteOne('users', { _id: ObjectID(req.params.id) })
+      res.status(200).send({ status: 'success' })
+    })
+
+    app.post('/authenticate', async (req, res) => {
+      const requiredParams = ['tenantId', 'username', 'password']
+
+      if (!objHasAllKeys(req.body, requiredParams)) {
+        return res.status(400).send({ error: 'missing required params', requiredParams })
+      }
+
+      const tenantId = req.body.tenantId
+      if (!tenantId || tenantId.length !== 24) {
+        return res.status(404).send({ error: 'invalid tenantId' })
+      }
+
+      const params = {
+        tenantId: ObjectID(req.body.tenantId),
+        username: req.body.username
+      }
+
+      const fetchedUser = await findOne('users', params)
+
+      if (!fetchedUser) {
+        return res.status(401).send({ error: 'unauthorized' })
+      }
+
+      const validPassword = await verifyPassword(req.body.password, fetchedUser.hashedPassword)
+      if (!validPassword) {
+        return res.status(401).send({ error: 'unauthorized' })
+      }
+
+      const payload = {
+        tenantId: fetchedUser.tenantId,
+        username: fetchedUser.username
+      }
+
+      const token = jwt.encode(payload, secret)
+
+      res.status(200).send({ status: 'success', token })
     })
 
     return app
@@ -142,8 +426,12 @@ function stopServer () {
   }
 }
 
+function decodeToken (token) {
+  return jwt.decode(token, secret)
+}
+
 async function cleanDb () {
-  console.info('Cleaning database (dropping collections)')
+  // console.info('Cleaning database (dropping collections)')
   await truncate('users')
   await truncate('tenants')
 }
@@ -151,5 +439,7 @@ async function cleanDb () {
 module.exports = {
   startServer,
   stopServer,
-  cleanDb
+  cleanDb,
+  findOne,
+  decodeToken
 }
